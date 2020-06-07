63c47d9d3aab5c474b7e87466689317448a276ec
kyuvie <alchemy.lapidis.philosophorum.13@gmail.com>
2020-06-07 16:40:17 +0900

    fix Main

--- src/main/java/b1u3/app/questionize/Main.java
+++ src/main/java/b1u3/app/questionize/Main.java
@@ -8,6 +8,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
@@ -26,18 +28,49 @@ public class Main {
         Questionizer qn = null;
         try {
             qn = new Questionizer(Paths.get(ns.<String> getList("file").get(0)));
-        } catch (IOException e) {
-            System.err.println("Creating Questionizer is failed");
+        } catch (Exception e) {
+            ap.printHelp();
             System.exit(0);
         }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Iterator<Question> qit = qn.iterator();
+        Integer all = 0;
+        Integer coverage = 0;
         while(qit.hasNext()) {
             Question q = qit.next();
+            all++;
             System.out.println(q.getStatement());
             q.shuffle();
+            int i = 0;
             for(String choice: q.getChoices()) {
-                System.out.println(choice);
+                System.out.println(i + ":" + choice);
+                i++;
+            }
+            Integer line = null;
+            while (true) {
+                try {
+                    System.out.print("Your answer:");
+                    line = Integer.valueOf(reader.readLine());
+                    if (!(0 <= line && line <= q.getChoices().size()-1)) {
+                        throw new NumberFormatException("Out of choice size!!");
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("Wrong format: Please input a number from 0 to " + (q.getChoices().size()-1));
+                    continue;
+                } catch (IOException e) {
+                    System.err.println("Input stream error");
+                    System.exit(0);
+                }
+            }
+            if (qn.ans(q, line)) {
+                System.out.println("OK");
+                coverage++;
+            } else {
+                System.out.println("NG");
             }
         }
+        // Out of Questions loop
+        System.out.println("Coverage: " + coverage + "/" + all);
     }
 }
